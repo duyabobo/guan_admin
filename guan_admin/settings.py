2fd0ca8e6dc27b18decfccf27884ba58c505fb8f
duyabo <duyabo@taou.com>
2020-03-06 20:23:13 +0800

    A guan

--- guan_admin/settings.py
+++ guan_admin/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'guan_admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'guan',
+        'USER': 'root',
+        'PASSWORD': 'duyabo',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
 
