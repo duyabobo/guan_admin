3f5e56545e60e6683511fbdd6de464410a3334da
duyabo <duyabo@taou.com>
2020-03-06 23:56:55 +0800

    A model.GuanGuan

--- guan/admin.py
+++ guan/admin.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from models import GuanGuan
 from models import GuanType
 
 
@@ -12,4 +13,11 @@ class GuanTypeAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
+class GuanGuanAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_time', 'updated_time')
+    list_display = ('name', 'guan_type', 'guan_point', 'status')
+    search_fields = ('name',)
+
+
 admin.site.register(GuanType, GuanTypeAdmin)
+admin.site.register(GuanGuan, GuanGuanAdmin)
--- guan/models.py
+++ guan/models.py
@@ -8,8 +8,26 @@ from django.db import models
 
 class GuanType(models.Model):
     name = models.CharField(max_length=50)
-    created_time = models.DateTimeField(default=datetime.now())
-    updated_time = models.DateTimeField(default=datetime.now())
+    created_time = models.DateTimeField(default=datetime.now)
+    updated_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "guan_type"
+
+    def __unicode__(self):
+        return self.name
+
+
+class GuanGuan(models.Model):
+    name = models.CharField(max_length=50)
+    guan_type = models.ForeignKey(GuanType)
+    guan_point = models.IntegerField(default=0)
+    status = models.IntegerField(default=0)
+    created_time = models.DateTimeField(default=datetime.now)
+    updated_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "guanguan"
+
+    def __unicode__(self):
+        return self.name
