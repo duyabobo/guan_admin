c23d62ae6d4fc6f352109903ab074cbce414c0ed
duyabo <duyabo@hundun.cn>
2022-09-25 16:04:36 +0800

    gitignore

--- guan_admin/settings.py
+++ guan_admin/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'guan',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'duyabo',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
