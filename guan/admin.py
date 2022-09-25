f30d4e71734ae8dd9a83aa2bd05f3829304ad465
duyabo <duyabo@hundun.cn>
2022-09-25 16:15:36 +0800

    Address

--- guan/admin.py
+++ guan/admin.py
@@ -3,52 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from models import AnswerInfo
-from models import GuanGuan
-from models import GuanInfo
-from models import GuanPoint
-from models import GuanType
-from models import OfflineMeeting
+from models import Address
 
 
-class GuanTypeAdmin(admin.ModelAdmin):
+class AddressAdmin(admin.ModelAdmin):
     readonly_fields = ('created_time', 'updated_time')
     list_display = ('id', 'name', 'created_time', 'updated_time')
     search_fields = ('name',)
 
 
-class GuanGuanAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_time', 'updated_time')
-    list_display = ('id', 'name', 'guan_type', 'guan_point', 'status')
-    search_fields = ('name',)
-
-
-class GuanInfoAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_time', 'updated_time')
-    list_display = ('id', 'question', 'guan')
-    search_fields = ('question',)
-
-
-class AnswerInfoAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_time', 'updated_time')
-    list_display = ('id', 'guan', 'guan_info', 'answer_key', 'answer_evaluation')
-    search_fields = ('answer_key',)
-
-
-class OfflineMeetingAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_time', 'updated_time')
-    list_display = ('id', 'guan', 'time', 'address', 'latitude', 'longitude')
-    search_fields = ('answer_key',)
-
-
-class GuanPointAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_time', 'updated_time')
-    list_display = ('id', 'guan', 'guan_point', 'user_id')
-
-
-admin.site.register(GuanType, GuanTypeAdmin)
-admin.site.register(GuanGuan, GuanGuanAdmin)
-admin.site.register(GuanInfo, GuanInfoAdmin)
-admin.site.register(AnswerInfo, AnswerInfoAdmin)
-admin.site.register(OfflineMeeting, OfflineMeetingAdmin)
-admin.site.register(GuanPoint, GuanPointAdmin)
+admin.site.register(Address, AddressAdmin)
--- guan/models.py
+++ guan/models.py
@@ -6,83 +6,17 @@ from datetime import datetime
 from django.db import models
 
 
-class GuanType(models.Model):
-    name = models.CharField(max_length=50)
-    created_time = models.DateTimeField(default=datetime.now)
-    updated_time = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = "guan_type"
-
-    def __unicode__(self):
-        return self.name
-
-
-class GuanGuan(models.Model):
-    name = models.CharField(max_length=50)
-    guan_type = models.ForeignKey(GuanType)
-    guan_point = models.IntegerField(default=0)
-    status = models.IntegerField(default=0)
-    created_time = models.DateTimeField(default=datetime.now)
-    updated_time = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = "guanguan"
-
-    def __unicode__(self):
-        return self.name
-
-
-class GuanInfo(models.Model):
-    question = models.CharField(max_length=200)
-    guan = models.ForeignKey(GuanGuan)
-    created_time = models.DateTimeField(default=datetime.now)
-    updated_time = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = "guan_info"
-
-    def __unicode__(self):
-        return self.question
-
-
-class AnswerInfo(models.Model):
-    guan = models.ForeignKey(GuanGuan)
-    guan_info = models.ForeignKey(GuanInfo)
-    answer_key = models.CharField(max_length=200)
-    answer_evaluation = models.CharField(max_length=50)
-    created_time = models.DateTimeField(default=datetime.now)
-    updated_time = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = "answer_info"
-
-    def __unicode__(self):
-        return self.answer_key
-
-
-class OfflineMeeting(models.Model):
-    guan = models.ForeignKey(GuanGuan)
-    time = models.DateTimeField(default=datetime.now)
-    address = models.CharField(max_length=200)
-    latitude = models.FloatField()
+class Address(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=32)
+    region_id = models.IntegerField(default=0)
     longitude = models.FloatField()
+    latitude = models.FloatField()
     created_time = models.DateTimeField(default=datetime.now)
     updated_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        db_table = "offline_meeting"
+        db_table = "address"
 
     def __unicode__(self):
-        return self.address
-
-
-class GuanPoint(models.Model):
-    guan = models.ForeignKey(GuanGuan)
-    guan_point = models.IntegerField()
-    user_id = models.IntegerField()
-    created_time = models.DateTimeField(default=datetime.now)
-    updated_time = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = "guan_point"
+        return self.name
