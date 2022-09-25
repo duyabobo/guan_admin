b96064ab6aa25137fed252b359cc6fdec1815776
duyabo <duyabo@hundun.cn>
2022-09-25 16:19:47 +0800

    Region

--- guan/admin.py
+++ guan/admin.py
@@ -3,13 +3,20 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from models import Address
+from models import Address, Region
 
 
 class AddressAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_time', 'updated_time')
-    list_display = ('id', 'name', 'created_time', 'updated_time')
+    readonly_fields = ('create_time', 'update_time', )
+    list_display = ('id', 'name', 'description', 'region_id', 'longitude', 'latitude', 'status', 'create_time', 'update_time',)
     search_fields = ('name',)
 
 
+# class RegionAdmin(admin.ModelAdmin):
+#     readonly_fields = ('create_time', 'update_time',)
+#     list_display = ('id', 'province', 'city', 'area', 'status', 'create_time', 'update_time',)
+#     search_fields = ('name',)
+
+
 admin.site.register(Address, AddressAdmin)
+# admin.site.register(Region, RegionAdmin)
--- guan/models.py
+++ guan/models.py
@@ -6,14 +6,30 @@ from datetime import datetime
 from django.db import models
 
 
+class Region(models.Model):
+    province = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    area = models.CharField(max_length=128)
+    status = models.IntegerField(default=1)
+    create_time = models.DateTimeField(default=datetime.now)
+    update_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "region"
+
+    def __unicode__(self):
+        return self.province + self.city + self.area
+
+
 class Address(models.Model):
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=32)
-    region_id = models.IntegerField(default=0)
+    region = models.ForeignKey(Region)
     longitude = models.FloatField()
     latitude = models.FloatField()
-    created_time = models.DateTimeField(default=datetime.now)
-    updated_time = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(default=1)
+    create_time = models.DateTimeField(default=datetime.now)
+    update_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "address"
